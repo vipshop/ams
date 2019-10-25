2dd90b53dc9a0e1f41016b88e3ab99cf914e362d
jun85.li <jun85.li@vipshop.com>
2019-10-25 10:20:59 +0800

    Updates

--- assets/js/167.93604907.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[167],{1145:function(t,e,n){"use strict";n.r(e);var l=n(1),s=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("ClientOnly",[e("market-template-preview")],1)],1)}),[],!1,null,null,null);e.default=s.exports}}]);
--- assets/js/169.13c6260c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[169],{123:function(n,w,o){}}]);
