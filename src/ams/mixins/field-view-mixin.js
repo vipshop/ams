c772954481825f8012bcc7723b16de01cb067b3a
ly525 <lantern.done@gmail.com>
2021-01-28 15:27:53 +0800

    eat: text field 支持渲染Vnode

--- src/ams/mixins/field-view-mixin.js
+++ src/ams/mixins/field-view-mixin.js
@@ -4,7 +4,10 @@ export default {
     computed: {
         on,
         viewValue() {
-            return this.field.view ? this.field.view(this.value, this.field, this.context) : this.value;
+            return this.field.view ? this.field.view(this.value, this.field, this.context, this.$createElement) : this.value;
+        },
+        htmlViewValue() {
+            return this.field.htmlView && this.field.htmlView(this.value, this.field, this.context, this.$createElement);
         }
     },
     inject: ['$block'],
