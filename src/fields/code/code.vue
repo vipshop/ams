27d9b977332aadfe9f0de1c1667c560596e400d6
ly525 <lantern.done@gmail.com>
2021-05-03 17:40:34 +0800

    feat(field): add code field with vue-codemirror

---
+++ src/fields/code/code.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="code-field-wrapper">
+        <codemirror
+            v-model="localValue" :options="cmOptions"
+    />
+    </div>
+</template>
+
+<script>
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/base16-dark.css';
+import { codemirror } from 'vue-codemirror'
+import mixins from '../../ams/mixins';
+
+export default {
+    mixins: [mixins.fieldEditMixin],
+    components: {
+        codemirror
+    },
+    data() {
+        return {
+            cmOptions: Object.freeze({
+                tabSize: 2,
+                mode: 'text/javascript',
+                theme: 'base16-dark',
+                lineNumbers: true,
+                line: true,
+            })
+        };
+    },
+};
+</script>
+<style lang="scss" scoped>
+.code-field-wrapper {
+    height: 100%;
+    box-sizing: border-box;
+    font-size: 14px;
+    line-height: 1.5;
+}
+</style>
