154112a6f3438b838c9845fe7e9658d963c66439
zebin.wu <zebin.wu@vipshop.com>
2021-06-01 21:28:23 +0800

    update docs
    update docs

---
+++ assets/js/170.259af13d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{1314:function(t,e,n){"use strict";n.r(e);var l=n(23),s=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("ClientOnly",[e("market-template-template")],1)],1)}),[],!1,null,null,null);e.default=s.exports}}]);
---
+++ docs/dist/assets/js/175.0cc52983.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[175],{1319:function(t,e,n){"use strict";n.r(e);var l=n(23),s=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("ClientOnly",[e("market-template-preview")],1)],1)}),[],!1,null,null,null);e.default=s.exports}}]);
